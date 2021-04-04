c24e3d59323b0b5dda023c32e9d87fad565af724
Majoxs <majoxs@gmail.com>
2021-04-04 18:39:50 +0500

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,25 @@
 $(function () {
 
+   //Звёздный рейтинг
 
+   $(".rate-star").rateYo({
+      rating: 5,
+      starWidth: "12px",
+      readOnly: true
+   });
 
+   //Слайдер
+
+   $('.product-slider__inner').slick({
+      dots: true,
+      arrows: false,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+   });
+
+   //Фильтр продуктов
+
+   var mixer = mixitup('.products__inner-box');
 
 
 });
