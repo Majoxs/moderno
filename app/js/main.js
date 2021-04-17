062bf4751e7844db468e9ffdf3024fa39203cbd8
Majoxs <majoxs@gmail.com>
2021-04-17 18:09:05 +0500

    added product-one page

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,48 @@
 $(function () {
 
-   //Звёздный рейтинг
+  //Звёздный рейтинг
 
-   $(".rate-star").rateYo({
-      rating: 5,
-      starWidth: "12px",
-      readOnly: true
-   });
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
 
-   //Слайдер
+  //Слайдер
 
-   $('.product-slider__inner').slick({
-      dots: true,
-      arrows: false,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-   });
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
 
-   //menu
+  //tabs
 
-   $('.menu__btn').on('click', function () {
-      $('.menu__list').slideToggle();
-   });
+  $('.product-one__tabs .tab').on('click', function (event) {
+    var id = $(this).attr('data-id');
+    $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+    $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#' + id).addClass('active-tab').fadeIn();
+    return false;
+  });
 
-   $('.header__btn-menu').on('click', function () {
-      $('.header__box').toggleClass('active');
-   });
+  //menu
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').slideToggle();
+  });
+
+  $('.header__btn-menu').on('click', function () {
+    $('.header__box').toggleClass('active');
+  });
 
 
 
-   //Фильтр продуктов
+  //Фильтр продуктов
 
-   var mixer = mixitup('.products__inner-box');
+  var mixer = mixitup('.products__inner-box');
 
 
 });
